7dee9f226fc6c85271f53849bce79a3b71868969
zgd6616 <50044309+zgd6616@users.noreply.github.com>
2019-10-09 16:19:58 +0800

    Update target.h

--- src/main/target/MATEKF411/target.h
+++ src/main/target/MATEKF411/target.h
@@ -21,42 +21,42 @@
 
 #define USBD_PRODUCT_STRING  "MatekF411"
 
-#define LED0                    PC13
-#define LED1                    PC14
+#define LED0                    PB4
+#define LED1                    PB5
 
-#define BEEPER                  PB2
+#define BEEPER                  
 #define BEEPER_INVERTED
 
-// *************** SPI1 Gyro & ACC **********************
+// *************** SPI2 Gyro & ACC **********************
 #define USE_SPI
-#define USE_SPI_DEVICE_1
+#define USE_SPI_DEVICE_2
 
-#define SPI1_SCK_PIN            PA5
-#define SPI1_MISO_PIN           PA6
-#define SPI1_MOSI_PIN           PA7
+#define SPI2_SCK_PIN            PB13
+#define SPI2_MISO_PIN           PB14
+#define SPI2_MOSI_PIN           PB15
 
 #define USE_GYRO_MPU6000
 #define USE_ACC_MPU6000
-#define MPU6000_CS_PIN          PA4
-#define MPU6000_SPI_BUS         BUS_SPI1
+#define MPU6000_CS_PIN          PB12
+#define MPU6000_SPI_BUS         BUS_SPI2
 #define GYRO_MPU6000_ALIGN      CW180_DEG
 #define ACC_MPU6000_ALIGN       CW180_DEG
 
 #define USE_GYRO_MPU6500
 #define USE_ACC_MPU6500
-#define MPU6500_CS_PIN          PA4
-#define MPU6500_SPI_BUS         BUS_SPI1
+#define MPU6500_CS_PIN          PB12
+#define MPU6500_SPI_BUS         BUS_SPI2
 #define GYRO_MPU6500_ALIGN      CW180_DEG
-#define ACC_MPU6500_ALIGN       CW180_DEG
+#define ACC_MPU6500_ALIGN       CW180_DEG           //？？？？？？？
 
 #define USE_EXTI
-#define GYRO_INT_EXTI            PA1
+#define GYRO_INT_EXTI           PA2                 //？？？？？？？
 #define USE_MPU_DATA_READY_SIGNAL
 
 #define USE_GYRO
 #define USE_ACC
 
-// *************** SPI2 OSD *****************************
+/*// *************** SPI2 OSD *****************************
 #define USE_SPI_DEVICE_2
 #define SPI2_SCK_PIN            PB13
 #define SPI2_MISO_PIN           PB14
@@ -66,20 +66,20 @@
 #define USE_MAX7456
 #define MAX7456_SPI_BUS         BUS_SPI2
 #define MAX7456_CS_PIN          PB12
-
+*/
 // *************** UART *****************************
 #define USE_VCP
-#define VBUS_SENSING_PIN        PC15
+#define VBUS_SENSING_PIN        PC15                //？？？？？？？
 #define VBUS_SENSING_ENABLED
 
 #define USE_UART1
 #define UART1_TX_PIN            PA9
 #define UART1_RX_PIN            PA10
-
+/*
 #define USE_UART2
 #define UART2_TX_PIN            PA2
 #define UART2_RX_PIN            PA3
-
+*/
 #if defined(MATEKF411_SFTSRL2)
 #define USE_SOFTSERIAL1
 #define SOFTSERIAL_1_TX_PIN     PA0 // ST1 pad
@@ -115,8 +115,8 @@
 // *************** I2C /Baro/Mag/Pitot ********************
 #define USE_I2C
 #define USE_I2C_DEVICE_1
-#define I2C1_SCL                PB8
-#define I2C1_SDA                PB9
+#define I2C1_SCL                PB6
+#define I2C1_SDA                PB7
 
 #define DEFAULT_I2C_BUS         BUS_I2C1
 
@@ -142,8 +142,8 @@
 #define USE_ADC
 #define ADC_INSTANCE                ADC1
 #define ADC1_DMA_STREAM             DMA2_Stream0
-#define ADC_CHANNEL_1_PIN           PB0
-#define ADC_CHANNEL_2_PIN           PB1
+#define ADC_CHANNEL_1_PIN           PA0
+#define ADC_CHANNEL_2_PIN           PA1
 #define VBAT_ADC_CHANNEL            ADC_CHN_1
 #define CURRENT_METER_ADC_CHANNEL   ADC_CHN_2
 
